7e440a9d82b6c7916fecb55ce3021c2185a6808a
Olga <olga.valor.wu@gmail.com>
2024-12-10 23:33:52 +0100

    lista

--- flutter/ud4_02_lista_horizontal/lib/lista_horizontal.dart
+++ flutter/ud4_02_lista_horizontal/lib/lista_horizontal.dart
@@ -15,26 +15,114 @@ class ListaHorizontal extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Text('Best Iranian Actors and Actress',
+            const Text('Best Iranian Actors and Actresses',
                 style: TextStyle(
                     color: Colors.black,
                     fontSize: 40,
                     fontWeight: FontWeight.bold)),
-            const Text('March 2020',
-                style: TextStyle(
-                  color: Color.fromARGB(87, 0, 0, 0),
-                  fontSize: 20,
-                )),
+            Container(
+              margin: const EdgeInsets.only(bottom: 30),
+              child: const Text('March 2020',
+                  style: TextStyle(
+                    color: Color.fromARGB(87, 0, 0, 0),
+                    fontSize: 15,
+                  )),
+            ),
             SizedBox(
-              height: 500, // constrain height
+              height: 500,
               child: ListView(
                   padding: const EdgeInsets.all(8),
                   scrollDirection: Axis.horizontal,
                   children: <Widget>[
                     Row(children: [
                       Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Image.asset('assets/img/emilia-clark.jpg'),
+                          Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(100),
+                              image: const DecorationImage(
+                                image: AssetImage(
+                                    'assets/images/emilia-clark.jpg'),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            height: 400,
+                            width: 200,
+                          ),
+                          const Text(
+                            'Emilia',
+                            style: TextStyle(fontSize: 30),
+                          ),
+                          const Text(
+                            'Clark',
+                            style: TextStyle(
+                                fontSize: 20,
+                                color: Color.fromARGB(88, 0, 0, 0)),
+                          )
+                        ],
+                      ),
+                      const SizedBox(
+                        width: 15,
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(100),
+                              image: const DecorationImage(
+                                image: AssetImage(
+                                    'assets/images/emilia-clark.jpg'),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            height: 400,
+                            width: 200,
+                          ),
+                          const Text(
+                            'Emilia',
+                            style: TextStyle(fontSize: 30),
+                          ),
+                          const Text(
+                            'Clark',
+                            style: TextStyle(
+                                fontSize: 20,
+                                color: Color.fromARGB(88, 0, 0, 0)),
+                          )
+                        ],
+                      ),
+                      const SizedBox(
+                        width: 15,
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(100),
+                              image: const DecorationImage(
+                                image: AssetImage(
+                                    'assets/images/emilia-clark.jpg'),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            height: 400,
+                            width: 200,
+                          ),
+                          const Text(
+                            'Emilia',
+                            style: TextStyle(fontSize: 30),
+                          ),
+                          const Text(
+                            'Clark',
+                            style: TextStyle(
+                                fontSize: 20,
+                                color: Color.fromARGB(88, 0, 0, 0)),
+                          )
                         ],
                       )
                     ])
